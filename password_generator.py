143ec29ba141f322c34ffa0b07423ddbed525732
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-06-20 19:48:26 +0500

    randomize a password length

--- password_generator.py
+++ password_generator.py
@@ -14,7 +14,7 @@ def password_generator(min_length, numbers=True, special_characters=True):
     pwd = ''
 
     while (any(d in pwd for d in digits) != numbers) and (any(s in pwd for s in special) != special_characters):
-        pwd = ''.join(random.sample(chars, min_length))
+        pwd = ''.join(random.sample(chars, random.randint(min_length, min_length+10)))
 
     return pwd
 
